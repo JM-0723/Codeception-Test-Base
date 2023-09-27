1ecf75a12f054bc3c530708523a50a42c32f5e55
JM0407 <april071995@gmail.com>
2023-09-28 02:36:53 +0800

    added data file

--- tests/Acceptance/TestCest.php
+++ tests/Acceptance/TestCest.php
@@ -14,13 +14,21 @@ class TestCest
     // tests
     public function tryToTest(AcceptanceTester $I)
     {
-        $scenario = $I->getScenarioObject();
-        $modules = $scenario->current('modules');
-        $helper = $modules['\Tests\Support\Helper\Acceptance'];
+        $scenario = $I->getScenarioObject();  
+        $modules = $scenario->current('modules'); 
+        $helper = $modules['\Tests\Support\Helper\Acceptance']; 
+        
 
-        $I->amOnPage('/');
+
+        //_getConfig function is use to get the data from the data file under _envs folder
+        $home = $helper->_getConfig('home');
+        $home_element = $helper->_getConfig('home_element');
+
+        //these are  the automated commands to be executed by the web driver
+        $I->amOnPage($home);
         $uri = $I->grabFromCurrentUrl();
-        echo($uri);
-        $I->seeElement("#hplogo");
+        $I->seeElement($home_element);
+
+
     }
 }
--- tests/Support/Helper/Acceptance.php
+++ tests/Support/Helper/Acceptance.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 namespace Tests\Support\Helper;
-
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
